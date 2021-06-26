c82824c961ea15914b52c7b66a0226d333764ace
JER.8789 <je.rogers8789@gmail.com>
2021-06-26 18:38:36 -0500

    run build files

---
+++ client/build/precache-manifest.8a79680479b6c73c958b6e52fbe8a446.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef74aaf269ae7f35b2db99f7ec136e6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f9866faacba9fde631d",
+    "url": "/static/css/2.cd554934.chunk.css"
+  },
+  {
+    "revision": "cb08438a3451460a8853",
+    "url": "/static/css/main.0b09ce91.chunk.css"
+  },
+  {
+    "revision": "9f9866faacba9fde631d",
+    "url": "/static/js/2.a765814f.chunk.js"
+  },
+  {
+    "revision": "e2a09bd6c72c2b5828bb73bece9e19fa",
+    "url": "/static/js/2.a765814f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb08438a3451460a8853",
+    "url": "/static/js/main.ac7d7ee3.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
